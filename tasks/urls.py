a07be31ea98979600bd848bad89054834bcf88c1
ryushade <77291154@usat.pe>
2024-07-09 22:38:19 -0500

    a

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,9 +3,8 @@ from rest_framework import routers
 from tasks import views
 
 router = routers.DefaultRouter()
-router.register(r'tasks', views.TaskView, 'task')
+router.register(r'tasks', views.TaskView, 'tasks')
 
 urlpatterns = [
     path("api/v1", include(router.urls))
-    
 ]
